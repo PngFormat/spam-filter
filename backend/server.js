63f797c0eb30ff6e3795a37a04d4c48c28d9896a
Denis <104354552+PngFormat@users.noreply.github.com>
2024-02-27 19:33:15 +0200

    spam-filter

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+});
+
+app.use(bodyParser.json());
 
 const messages = [];
 
 io.on('connection', (socket) => {
     console.log('Client connected');
 
-    // Отримання повідомлень при підключенні
     socket.emit('initialMessages', messages);
 
-    // Обробка нового повідомлення та відправка його всім клієнтам
     socket.on('sendMessage', (newMessage) => {
         const formattedMessage = { id: messages.length + 1, text: newMessage.text };
         messages.push(formattedMessage);
@@ -26,6 +34,24 @@ io.on('connection', (socket) => {
     });
 });
 
+app.get('/api/messages', (req, res) => {
+
+    res.json(messages);
+});
+
+
+app.post('/api/messages', (req, res) => {
+    const { text } = req.body;
+    const newMessage = { id: messages.length + 1, text };
+    messages.push(newMessage);
+    io.emit('newMessage', newMessage);
+    res.json(newMessage);
+});
+
 server.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
+
+app.options('/api/messages', cors());
+app.use(bodyParser.json());
+app.use(cors());
